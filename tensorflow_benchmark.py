591560f10aa2142942937b75f5510458164a0930
Pedro Holanda <pedroholanda@gmail.com>
2019-02-18 21:01:13 +0100

    Cleaning TF code and performing date filters inside functions

--- tensorflow_benchmark.py
+++ tensorflow_benchmark.py
@@ -7,22 +7,24 @@ os.chdir('tpch-dbgen')
 
 lineitem = np.genfromtxt("lineitem.tbl",delimiter='|', names=["l_orderkey","l_partkey","l_suppkey","l_linenumber","l_quantity","l_extendedprice","l_discount","l_tax","l_returnflag","l_linestatus","l_shipdate","l_commitdate","l_receiptdate","l_shipinstruct","l_shipmode","l_comment"],dtype=['int32','int32','int32','int32','float64','float64','float64','float64','S1','S1','S10','S10','S10','S25','S10','S44'])
 
+def to_integer(dt_time):
+  dt_time = dt_time.split("-")
+  return 10000*int(dt_time[0]) + 100*int(dt_time[1]) + int(dt_time[2])
+
+vfunc = np.vectorize(to_integer)
+l_shipdate = vfunc(lineitem['l_shipdate'])
+
 def q6(): 
-  filter_shipdate = tf.placeholder(dtype=tf.bool, shape=(None,))
+  shipdate = tf.placeholder(dtype=tf.int32, shape=(None,))
   discount = tf.placeholder(dtype=tf.float64, shape=(None,))
   quantity = tf.placeholder(dtype=tf.float64, shape=(None,))
   extendedprice = tf.placeholder(dtype=tf.float64, shape=(None,))
-  complete_filter = tf.logical_and(tf.greater_equal(discount,0.05),tf.logical_and(tf.less_equal(discount,0.07),tf.logical_and(tf.less(quantity,24),filter_shipdate)))
-  a = tf.boolean_mask(extendedprice,complete_filter)
-  b = tf.boolean_mask(quantity,complete_filter)
-  c = tf.multiply(tf.boolean_mask(extendedprice,complete_filter), tf.boolean_mask(discount,complete_filter))
-  d = tf.reduce_sum(c)
+  zeros = tf.zeros_like(discount)
+  complete_filter = tf.logical_and(tf.greater_equal(discount,0.05),tf.logical_and(tf.less_equal(discount,0.07),tf.logical_and(tf.less(quantity,24),tf.logical_and(tf.less(shipdate,19950101),tf.greater_equal(shipdate,19940101)))))
+  result = tf.reduce_sum(tf.multiply(tf.where(complete_filter,extendedprice,zeros), tf.where(complete_filter,discount,zeros)))
   with tf.Session() as sess:
-    # Tensorflow does not support filters on strings
-    filter_1 = lineitem['l_shipdate'] >="1994-01-01"
-    filter_2 = lineitem['l_shipdate'] < "1995-01-01"
-    res = sess.run(d, feed_dict={
-        filter_shipdate: filter_1&filter_2,
+    res = sess.run(result, feed_dict={
+        shipdate: l_shipdate,
         discount: lineitem['l_discount'],
         quantity: lineitem['l_quantity'],
         extendedprice: lineitem['l_extendedprice']
--- tensorflow_tpu_benchmark.py
+++ tensorflow_tpu_benchmark.py
@@ -6,26 +6,21 @@ from tensorflow.contrib import tpu
 from tensorflow.contrib.cluster_resolver import TPUClusterResolver
 os.chdir('tpch-dbgen')
 lineitem = np.genfromtxt("lineitem.tbl",delimiter='|', names=["l_orderkey","l_partkey","l_suppkey","l_linenumber","l_quantity","l_extendedprice","l_discount","l_tax","l_returnflag","l_linestatus","l_shipdate","l_commitdate","l_receiptdate","l_shipinstruct","l_shipmode","l_comment"],dtyp$
-def q6_computation(filter_shipdate,discount,quantity,extendedprice):
-#  filter_shipdate = True #tf.not_equal(filter_shipdate,0)
-  aux = tf.constant([0.05],dtype=tf.float32)
-  filter_3 = tf.greater_equal(discount,aux)
-  aux = tf.constant([0.07],dtype=tf.float32)
-  filter_4 = tf.less_equal(discount,aux)
-  aux = tf.constant([24],dtype=tf.float32)
-  filter_5 = tf.less_equal(quantity,aux)
-  complete_filter = tf.logical_and(filter_shipdate,tf.logical_and(filter_3,tf.logical_and(filter_4,filter_5)))
-  extendedprice = tf.boolean_mask(extendedprice,complete_filter)
-#  quantity = tf.boolean_mask(quantity,complete_filter)
-  c = tf.multiply(extendedprice, quantity)
-  d = tf.reduce_sum(c)
+
+def to_integer(dt_time):
+  dt_time = dt_time.split("-")
+  return 10000*int(dt_time[0]) + 100*int(dt_time[1]) + int(dt_time[2])
+
+vfunc = np.vectorize(to_integer)
+l_shipdate = vfunc(lineitem['l_shipdate'])
+
+def q6_computation(shipdate,discount,quantity,extendedprice):
+  zeros = tf.zeros_like(discount)
+  complete_filter = tf.logical_and(tf.greater_equal(discount,0.05),tf.logical_and(tf.less_equal(discount,0.07),tf.logical_and(tf.less(quantity,24),tf.logical_and(tf.less(shipdate,19950101),tf.greater_equal(shipdate,19940101)))))
+  result = tf.reduce_sum(tf.multiply(tf.where(complete_filter,extendedprice,zeros), tf.where(complete_filter,discount,zeros)))
+
 def q6(): 
-  filter_1 = lineitem['l_shipdate'] >="1994-01-01"
-  filter_2 = lineitem['l_shipdate'] < "1995-01-01"
- # inputs = [ tf.constant(filter_1&filter_2,dtype=tf.bool), tf.constant(lineitem['l_discount'],dtype=tf.float64), tf.constant(lineitem['l_quantity'],dtype=tf.float64), tf.constant(lineitem['l_extendedprice'],dtype=tf.float64)]
-  #inputs = [tf.constant(lineitem['l_quantity'],dtype=tf.float64), tf.constant(lineitem['l_extendedprice'],dtype=tf.float64)]
-  #inputs = [2.0,5.0]
-  inputs = [tf.convert_to_tensor(filter_1 & filter_2, np.bool),tf.convert_to_tensor(lineitem['l_discount'], np.float32),tf.convert_to_tensor(lineitem['l_quantity'], np.float32),tf.convert_to_tensor(lineitem['l_quantity'], np.float32)]
+  inputs = [tf.convert_to_tensor(shipdate, np.int32),tf.convert_to_tensor(lineitem['l_discount'], np.float32),tf.convert_to_tensor(lineitem['l_quantity'], np.float32),tf.convert_to_tensor(lineitem['l_quantity'], np.float32)]
   tpu_computation = tpu.rewrite(q6_computation, inputs)
   tpu_grpc_url = TPUClusterResolver(
     tpu=[os.environ['TPU_NAME']]).get_master()
@@ -35,11 +30,7 @@ def q6():
     res = sess.run(tpu_computation)
     sess.run(tpu.shutdown_system())
   return res
-n = 5
+
 start_time = timeit.default_timer()
 q6()
 print(timeit.default_timer() - start_time)
-#def bench(q):
-#  res = np.median(timeit.repeat("q6()", number=1, repeat=n))
-#  print(res)
-#bench(6)
